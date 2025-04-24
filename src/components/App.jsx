36327fde7f0241df0ce55e0bb3f24335293369fa
sanchousser <golyukaleks@gmail.com>
2025-04-24 19:15:04 +0200

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,43 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import { Component } from "react";
+import { Section } from "./Section/Section";
+
+export class App extends Component {
+
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  }
+
+  handleBtnFeedback = (name) => {
+    this.setState((prevState) => ({
+      [name]: prevState[name] + 1
+    }))
+  }
+
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state
+
+    return (good + neutral + bad)
+  }
+
+  countPositiveFeedbackPercentage = () => {
+    const { good } = this.state
+    const totalFeedback = this.countTotalFeedback()
+    const positiveFeedback = Math.round((good / totalFeedback) * 100)
+    return (positiveFeedback ? positiveFeedback : 0)
+  }
+
+  render() {
+    return (
+      <>
+        <Section title='Please leave Feedback'>
+          {/* <FeedbackOption />*/}
+        </Section>
+        <Section title='Statistics'>
+          {/* {this.countTotalfeedback() > 0 ? <Statistics />} : <Notification message='There is no feedback'/> */}
+        </Section>
+      </>
+    )
+  }
 };
