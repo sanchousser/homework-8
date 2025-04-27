ed017a5c88f9f024099c72acb1f39b3640ad64d2
sanchousser <golyukaleks@gmail.com>
2025-04-27 11:38:07 +0200

    upd

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -3,7 +3,7 @@ import css from './Section.module.css'
 
 export const Section = ({title, children}) => {
     return(
-        <section>
+        <section className={css.section}>
             <h1>{title}</h1>
             {children}
         </section>
